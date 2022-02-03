7bbc6373452b7f1f6dcbd276fcb6b6ed9379e7f5
EC2 Default User <ec2-user@ip-172-31-22-173.ec2.internal>
2022-02-03 12:54:21 +0000

    [Update]4

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,29 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+ #follower_id: するほう # followed_id: されるほう
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
-  has_many :followers, class_name: "relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followers_list, through: :followers, source: :relationship
-
-  has_many :followeds, class_name: "relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followeds_list, through: :followeds, source: :relationship
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
 
   validates :introduction, length: {maximum: 50}
+
+
+  def follow(user_id)
+    self.relationships.create!(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    self.followings.include?(user)
+  end
 
   def get_profile_image(size)
     unless profile_image.attached?
